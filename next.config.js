18f5ff36170fa0d7b4cd6f95c6fd5162f759617c
Krzysztof_Karowiec <krzysztof.karowiec@gmail.com>
2023-01-13 11:05:03 +0100

    updated config files, removed unused files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    // to powinno zwiększać wydajność apki modyfikując importy ikon fontawesome ale nie bangla :c
+    // modularizeImports: {
+    //     '@fortawesome/free-regular-svg-icons': {
+    //         transform: '@fortawesome/free-regular-svg-icons/{{member}}',
+    //     },
+    //     '@fortawesome/free-solid-svg-icons': {
+    //         transform: '@fortawesome/free-solid-svg-icons/{{member}}',
+    //     }
+    // }
+    //języki:
+    // i18n: {
+    //     locales: ['en', 'pl'],
+    //     defaultLocale: 'en',
+    // }
 }
 
 module.exports = nextConfig
