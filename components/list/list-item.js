77aa6538c15c820913b91786ad2a2e0b7d7c52c5
Krzysztof_Karowiec <krzysztof.karowiec@gmail.com>
2023-02-02 18:05:13 +0100

    added feature: copying products from existing lists to new list

--- components/list/list-item.js
+++ components/list/list-item.js
@@ -1,6 +1,17 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const ListItem = (props) => {
+    const [checked, setChecked] = useState(props.checked);
+
+    const clickHandler = () => {
+        if (checked) {
+            props.uncheck(props._id);
+        } else {
+            props.check(props._id);
+        }
+        setChecked(prev => !prev);
+    }
 
     return (
         <motion.div
@@ -10,7 +21,13 @@ const ListItem = (props) => {
             animate={{ scale: 1, opacity: 1, y: "0%" }}
             exit={{ scale: 0.9, opacity: 0, x: "10%" }}
             transition={{ type: "spring", duration: 0.5, x: { duration: 0.1 } }}>
-            <div className="flex p-1.5"><p className="w-10 items-center justify-center text-cyan-400 flex text-2xl font-semibold border-r border-cyan-400 mr-3">{props.index + 1}</p><p className="grow"><strong>{props.name}</strong><br />ilość: {props.amount}</p></div>
+            <div className="flex p-1.5">
+                <p className="w-10 items-center justify-center text-cyan-400 flex flex-col text-xl font-semibold border-r border-cyan-400 mr-3">
+                    <span>{props.index + 1}</span>
+                    <input type="checkbox" className=" accent-cyan-400 text-white" checked={props.checked} onClick={clickHandler} />
+                </p>
+                <p className="grow"><strong>{props.name}</strong><br />ilość: {props.amount}</p>
+            </div>
         </motion.div>
     );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Alerts from '../components/alert/alerts';
 import Layout from '../components/layout/layout'
 import AlertContextProvider from '../components/store/alerts-context';
+import CartContextProvider from '../components/store/cart-context';
 import ProductsContextProvider from '../components/store/products-context';
 import '../styles/globals.css'
 
@@ -8,10 +9,12 @@ export default function App({ Component, pageProps }) {
     return (
         <AlertContextProvider>
             <ProductsContextProvider>
-                <Layout>
-                    <Component {...pageProps} />
-                    <Alerts />
-                </Layout>
+                <CartContextProvider>
+                    <Layout>
+                        <Component {...pageProps} />
+                        <Alerts />
+                    </Layout>
+                </CartContextProvider>
             </ProductsContextProvider>
         </AlertContextProvider>
     );
